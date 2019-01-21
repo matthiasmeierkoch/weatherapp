4fbe3c24f3b1b6befb20c6551634539acb9f75cd
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-01-21 12:56:19 +0100

    positioning and styling of min and max temerature.

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let key = '78c7ef3471574b8f89492311191101'; // signup https://www.apixu.com/sign
 let cities = ['Zürich', 'Sydney', 'London', 'New York'];
 let i = 0;
 
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
@@ -19,7 +20,7 @@ function gotWeather(weather) {
 }
 
 function reloadJson() {
-    ort = input.value();
+    ort = drawText(cities.values(i));
     url = 'https://api.apixu.com/v1/forecast.json?key=78c7ef3471574b8f89492311191101&q=' + ort + '&days=7';
     loadJSON(url, gotWeather);//nachdem das json File geladen ist, rufen wir die Funktion gotWeather auf
 }
@@ -28,6 +29,8 @@ function draw() {
     background(51);
     drawCities();
     drawText();
+    drawmaxTemp();
+    drawminTemp();
 }
 
 function drawCities() {
@@ -44,14 +47,46 @@ function drawCities() {
 function drawText() {
     fill(255);
     noStroke();
-    textSize(22);
-        text(cities[i], width / 2, height / 2 - height / 4);
+    textSize(26);
+    text(cities[i], width / 2, height / 2 - height / 4);
 }
 
 function mousePressed() {
     i = i + 1;
 
-    if (i == cities.length){
+    if (i == cities.length) {
         i = 0;
+    }
+    reloadJson();
+}
+
+
+function drawmaxTemp() {
+
+    let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
+    fill(255, 0, 0, 80);
+    noStroke();
+    ellipse(width / 2, height / 2, 300, 300);
+
+    for (let s = 0; s < days; s++) {
+        fill(255);
+        noStroke();
+        textSize(20);
+        text(weatherdays[s].day.maxtemp_c, width / 2, height / 2);//wir geben hier die Maximaltemperatur aus
+    }
+}
+
+function drawminTemp() {
+
+    let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
+    fill(255, 0, 0, 80);
+    noStroke();
+    ellipse(width / 2, height / 2 + 200, 300, 300);
+
+    for (let s = 0; s < days; s++) {
+        fill(255);
+        noStroke();
+        textSize(20);
+        text(weatherdays[s].day.mintemp_c, width / 2, height / 2 + 200);//wir geben hier die Maximaltemperatur aus
     }
 }
