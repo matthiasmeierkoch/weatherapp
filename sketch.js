6be455c9403cdaae3bcbfcd1ab796634a42cc9ab
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-01-24 19:16:13 +0100

    temperatur abhängige positionierung

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let weatherdays = []; //in dieses Array füllen wir die Wettervorschau der komme
 let key = '78c7ef3471574b8f89492311191101'; // signup https://www.apixu.com/signup.aspx
 let cities = ['Zürich', 'Sydney', 'London', 'New York'];
 let i = 0;
+let d = 600;
 
 
 function setup() {
@@ -32,11 +33,14 @@ function draw() {
     drawminTemp();
     drawSunset();
     drawSunrise();
+    drawMinMaxTemp();
 }
 
 function drawCities() {
+
     let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
     let angle = 360 / days;// Hier rechnen wir den Drehwinkel, damit das mit der Anzahl Tage schön aufgeht
+
     noFill();
     stroke(255);
     strokeWeight(3);
@@ -53,44 +57,64 @@ function drawText() {
 
 function mousePressed() {
     i = i + 1;
+
     if (i == cities.length) {
         i = 0;
     }
     reloadJson();
 }
 
+
 function drawmaxTemp() {
+
     let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
-    fill(255, 0, 0, 80);
-    noStroke();
-    ellipse(width / 2 - 160, height / 2, 200, 200);
+
+
     for (let s = 0; s < days; s++) {
+        let maxTemp = weatherdays[0].day.maxtemp_c;
+
+        fill(255, 0, 0, 80);
+        noStroke();
+        ellipse(width / 2 - 160, height / 2 - maxTemp, 200, 200);
+
         fill(255);
         noStroke();
         textSize(20);
-        text(weatherdays[0].day.maxtemp_c, width / 2 - 160, height / 2);//wir geben hier die Maximaltemperatur aus
+        text(weatherdays[0].day.maxtemp_c, width / 2 - 160, height / 2 - maxTemp);//wir geben hier die Maximaltemperatur aus
+
+
     }
 }
 
 
 function drawminTemp() {
+
     let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
-    fill(255, 0, 0, 80);
-    noStroke();
-    ellipse(width / 2 - 160, height / 2 + 150, 200, 200);
+
+
     for (let s = 0; s < days; s++) {
+        let minTemp = weatherdays[0].day.mintemp_c;
+
+        fill(255, 0, 0, 80);
+        noStroke();
+        ellipse(width / 2 - 160, height / 2 + 160 - minTemp , 200, 200);
+
         fill(255);
         noStroke();
         textSize(20);
-        text(weatherdays[0].day.mintemp_c, width / 2 - 160, height / 2 + 160);//wir geben hier die Maximaltemperatur aus
+        text(weatherdays[0].day.mintemp_c, width / 2 - 160, height / 2 + 160 - minTemp);//wir geben hier die Maximaltemperatur aus
+
+
     }
 }
 
 function drawSunrise() {
+
     let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
     fill(255, 0, 0, 80);
     noStroke();
     ellipse(width / 2 + 160, height / 2, 200, 200);
+
     for (let s = 0; s < days; s++) {
         fill(255);
         noStroke();
@@ -100,10 +124,12 @@ function drawSunrise() {
 }
 
 function drawSunset() {
+
     let days = weatherdays.length;//Hier fragen wir ab,  wieviele Tage im Array weatherdays gespeichert sind
     fill(255, 0, 0, 80);
     noStroke();
     ellipse(width / 2 + 160, height / 2 + 150, 200, 200);
+
     for (let s = 0; s < days; s++) {
         fill(255);
         noStroke();
@@ -117,4 +143,24 @@ function drawDate() {
     noStroke();
     textSize(20);
     text(weatherdays[0].date, width / 2,height / 2 - 150);// current date
+}
+
+function drawMinMaxTemp (){
+    let days = weatherdays.length;
+
+    for (let s = 0; s < days; s++) {
+        let minTemp = weatherdays[0].day.mintemp_c;
+        let maxTemp = weatherdays[0].day.maxtemp_c;
+
+        let xPos = map(minTemp, -40, 40, (d - 100) / 2, (d + 100) / 2);
+        fill(0, 0, 255, 100);
+        noStroke();
+        ellipse(xPos, 0, 5, 5); //Minimal Temperatur
+
+        xPos = map(maxTemp, -40, 40, (d - 100) / 2, (d + 100) / 2);
+        fill(255, 0, 0, 100);
+        ellipse(xPos, 0, 5, 5); //Maximal Temperatur
+        noFill();
+        stroke(1)
+    }
 }
